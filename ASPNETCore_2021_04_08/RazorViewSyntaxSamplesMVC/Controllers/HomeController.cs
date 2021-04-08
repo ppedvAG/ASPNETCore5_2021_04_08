12537d42f9e4dfd208aa5ebe2ecf369e2b8a5e4a
Kevin-Winter-Codes <Kevin.Winter.Privat@gmail.com>
2021-04-08 17:19:44 +0200

    Razor Syntax

---
+++ ASPNETCore_2021_04_08/RazorViewSyntaxSamplesMVC/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using DI_BasicSample;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RazorViewSyntaxSamplesMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorViewSyntaxSamplesMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult ShowCar()
+        {
+            ICar car = new MockCar();
+            return View(car);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
